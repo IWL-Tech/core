684fed8acef82c879a76616c78d88f3d125e7e23
root <root@localhost.localdomain>
2022-06-02 09:33:09 +0000

    Stuff

---
+++ index.mjs
@@ -0,0 +1,86 @@
+import http from 'http'
+import tweetnacl from 'tweetnacl';
+import fetch from 'node-fetch'
+
+// Your public key can be found on your application in the Developer Portal
+const PUBLIC_KEY = '0b91c5aee984a385dd9453eb591668c9d481a018ba12e7213c627aa4c184c58a';
+
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+http.createServer(app).listen(port, '127.0.0.1')
+
+app.get('/', (req, res) => {
+  res.send('IWL API')
+	console.log("here")
+})
+
+app.post('/evaluator', (req, res) => {
+  const signature = req.get('X-Signature-Ed25519');
+  const timestamp = req.get('X-Signature-Timestamp');
+  const body = req.rawBody; // rawBody is expected to be a string, not raw bytes
+
+  const isVerified = tweetnacl.sign.detached.verify(
+    Buffer.from(timestamp + body),
+    Buffer.from(signature, 'hex'),
+    Buffer.from(PUBLIC_KEY, 'hex')
+  );
+
+  if (!isVerified) {
+    return res.status(401).end('invalid request signature');
+  }
+
+  body = req.body
+
+  switch (body.type) {
+    case '1':
+      return res.status(200).send(JSON.stringify({type: 1}))
+
+    case '2':
+      res.send(JSON.stringify({type: 2, data: {content: "Hello World"}}))
+  }
+})
+
+app.get('/evaluator/init', async function(req, res)  {
+  fetch('https://discord.com/api/v10/applications/977803682534019082/commands', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: "changelog",
+      description: "Publishes a new changelog. Must have Manage Events to use.",
+      default_member_permissions: 8589934592
+    }),
+    headers: {
+      Authorization: "Bot OTc3ODAzNjgyNTM0MDE5MDgy.GiaGvM.3C1FhEHM8xYlawtqUV0EACBbLhbIZ9UzDcEAVo",
+      "Content-Type": "application/json"
+    }
+  })
+
+  fetch('https://discord.com/api/v10/applications/977803682534019082/commands', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: "repo",
+      description: "Sends you a link to the Evaluate GitHub repository."
+    }),
+    headers: {
+      Authorization: "Bot OTc3ODAzNjgyNTM0MDE5MDgy.GiaGvM.3C1FhEHM8xYlawtqUV0EACBbLhbIZ9UzDcEAVo",
+      "Content-Type": "application/json"
+    }
+  })
+
+  fetch('https://discord.com/api/v10/applications/977803682534019082/commands', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: "download",
+      description: "Sends you a link to the Evaluate Sourceforge page."
+    }),
+    headers: {
+      Authorization: "Bot OTc3ODAzNjgyNTM0MDE5MDgy.GiaGvM.3C1FhEHM8xYlawtqUV0EACBbLhbIZ9UzDcEAVo",
+      "Content-Type": "application/json"
+    }
+  })
+
+  res.status(200).send("Commands initialised")
+})
+
